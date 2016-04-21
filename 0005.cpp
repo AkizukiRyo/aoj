a223d016cdaa3c1d57f1d4f4071fa44e2529eb5f
unknown <>
2016-04-21 20:42:17 +0900

    update
    アルゴリズムの変更

--- 0005.cpp
+++ 0005.cpp
@@ -1,29 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 using namespace std;
 
+int Gcd(int a, int b)
+{
+	//ユークリッドの互除法
+	if (a < b) swap(a, b);
+	if (b == 0) return a;
+	return Gcd(a % b, b);
+}
+
 int main()
 {
-	long int a, b;
+	int a, b;
 	while (cin >> a >> b)
 	{
-		if (a < b) swap(a, b);
-		for (int i = 1; i <= a; i++)
-		{
-			if (a % i == 0 && b % (a / i) == 0)
-			{
-				cout << a / i << " ";
-				break;
-			}
-		}
-		for (int i = 1; i <= b; i++)
-		{
-			if ((a * i) % b == 0)
-			{
-				cout << a * i << endl;
-				break;
-			}
-		}
+		int gcd = Gcd(a, b);
+		int lcm = a / gcd * b;
+		cout << gcd << " " << lcm << endl;
 	}
 
 	return 0;
